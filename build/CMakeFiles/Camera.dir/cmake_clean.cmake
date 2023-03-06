cbfe2539000335a2cfcde887b914df4ba04b0974
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-03-07 00:52:11 +0530

    Camera for Linux

---
+++ build/CMakeFiles/Camera.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Camera.dir/src/main.cc.o"
+  "CMakeFiles/Camera.dir/src/main.cc.o.d"
+  "Camera"
+  "Camera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Camera.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Camera.
+# This may be replaced when dependencies are built.
